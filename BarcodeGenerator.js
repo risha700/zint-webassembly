8992c6c35919b1cfadfda717b2d9a0a6a84077c9
risha700 <ahbox@outlook.com>
2021-05-16 18:27:14 +0400

    added download and print buttons

--- BarcodeGenerator.js
+++ BarcodeGenerator.js
@@ -7,12 +7,21 @@ export default class BarcodeGenerator{
         this.barcodeType = document.createElement('select')
         this.barcodeScale = document.createElement('input')
         this.button = document.createElement('button')
+        this.bmp_data = null
+        this.img = document.getElementsByTagName('img')[0]|| new Image();
+        this.output_wrapper = document.createElement('div')
+        this.download_btn = document.createElement('button')
+        this.print_btn = document.createElement('button')
         this.errorText = document.createElement('p')
         this.init()
     }
     init(){
         this.buildUI()
         this.initWorker()
+        this.initEvents()
+
+    }
+    initEvents(){
         this.button.addEventListener('click', (evt)=>{
             this.requestBarcode(evt, {
                 'barcodeType':this.barcodeType.value,
@@ -21,6 +30,17 @@ export default class BarcodeGenerator{
             })
         })
 
+        this.download_btn.addEventListener('click',()=>{
+            window.URL.revokeObjectURL(this.prepareDownloadUrl())
+        })
+        this.print_btn.addEventListener('click', ()=>{
+            let popup = window.open(this.img.src,"_new")
+            popup.onafterprint =()=> popup.close() 
+            popup.focus()
+            popup.print()
+
+
+        })
     }
     buildUI(){
         this.barcodeText.setAttribute('type', 'text')
@@ -34,20 +54,25 @@ export default class BarcodeGenerator{
         this.barcodeScale.setAttribute('max', '6')
         // button
         this.button.innerHTML = "Generate Barcode"
+        this.download_btn.innerHTML = "&#x1f4be;"
+        this.print_btn.innerHTML = "&#128424;"
+
         
         this.wrapper.appendChild(this.barcodeType)
         this.wrapper.appendChild(this.barcodeText)
         this.wrapper.appendChild(this.barcodeScale)
         this.wrapper.appendChild(this.button)
         this.errorText.setAttribute('style', 'color:red;')
+        this.output_wrapper.setAttribute('class', 'output_wrapper')
     }
     initWorker(){
         if(this.worker === null) this.worker = new Worker(this.processor_path);
         this.worker.onmessage =(e)=> this.workerOnMessage(e)
     }
     workerOnMessage(e){
-        this.handleCErrors(e.data)
+        if(!this.hasCErrors(e.data))
         this.outputBMP(e.data)
+
     }
     requestBarcode(e, data){
         e.preventDefault();
@@ -68,11 +93,28 @@ export default class BarcodeGenerator{
         // arr = new Uint8Array(header_size + image_size);
         const view = new DataView(arr.buffer);
         this.structBmpDataView(view, arr, header_size, width, height, image_size);
+        this.bmp_data = arr
         const blob = new Blob([arr], { type: "image/png" });
         const url = window.URL.createObjectURL(blob);
-        const img =document.getElementsByTagName('img')[0]|| new Image();
-        img.src = url;
-        this.wrapper.appendChild(img)   // needs a change 
+        // const img =document.getElementsByTagName('img')[0]|| new Image();
+        this.img.src = url;
+        this.showOutputFrame(this.img, data)
+
+    }
+    prepareDownloadUrl(){
+        let url = window.URL.createObjectURL(new Blob([this.bmp_data], {type: "image/octet-stream"}))
+        let a = document.createElement('a')
+        a.setAttribute("href", url);
+        a.setAttribute("download", `${this.barcodeText.value}.png`)
+        a.click()
+        return url
+    }
+    showOutputFrame(img, data){
+        this.output_wrapper.appendChild(img)   // needs a change 
+        this.output_wrapper.appendChild(this.download_btn)
+        this.output_wrapper.appendChild(this.print_btn)
+        this.wrapper.appendChild(this.output_wrapper)
+
     }
     structBmpDataView(view, arr, header_size, width, height, image_size){
         view.setUint16(0, 0x424D, false); // BM magic number.
@@ -112,23 +154,30 @@ export default class BarcodeGenerator{
         return imageData;
         }
 
-        generateBarcodeTypes(){
-            const BARCODE_TYPES = {1: 'BARCODE_CODE11 Code 1', 2: 'BARCODE_C25MATRIX Standard Code 2 of ', 3: 'BARCODE_C25INTER Interleaved 2 of ', 4: 'BARCODE_C25IATA Code 2 of 5 IAT', 6: 'BARCODE_C25LOGIC Code 2 of 5 Data Logi', 7: 'BARCODE_C25IND Code 2 of 5 Industria', 8: 'BARCODE_CODE39 Code 3 of 9 (Code 39', 9: 'BARCODE_EXCODE39 Extended Code 3 of 9 (Code 39+', 13: 'BARCODE_EANX EA', 16: 'BARCODE_EAN128 GS1-12', 18: 'BARCODE_CODABAR Codaba', 20: 'BARCODE_CODE128 Code 128 (automatic subset switching', 21: 'BARCODE_DPLEIT Deutshe Post Leitcod', 22: 'BARCODE_DPIDENT Deutshe Post Identcod', 23: 'BARCODE_CODE16K Code 16', 24: 'BARCODE_CODE49 Code 4', 25: 'BARCODE_CODE93 Code 9', 28: 'BARCODE_FLAT Flattermarke', 29: 'BARCODE_RSS14 GS1 DataBar-1', 30: 'BARCODE_RSS_LTD GS1 DataBar Limite', 31: 'BARCODE_RSS_EXP GS1 DataBar Expande', 32: 'BARCODE_TELEPEN Telepen Alph', 34: 'BARCODE_UPCA UPC ', 37: 'BARCODE_UPCE UPC ', 40: 'BARCODE_POSTNET PostNe', 47: 'BARCODE_MSI_PLESSEY MSI Plesse', 49: 'BARCODE_FIM FI', 50: 'BARCODE_LOGMARS LOGMAR', 51: 'BARCODE_PHARMA Pharmacode One-Trac', 52: 'BARCODE_PZN PZ', 53: 'BARCODE_PHARMA_TWO Pharmacode Two-Trac', 55: 'BARCODE_PDF417 PDF41', 56: 'BARCODE_PDF417TRUNC PDF417 Truncate', 57: 'BARCODE_MAXICODE Maxicod', 58: 'BARCODE_QRCODE QR Cod', 60: 'BARCODE_CODE128B Code 128 (Subset B', 63: 'BARCODE_AUSPOST Australia Post Standard Custome', 66: 'BARCODE_AUSREPLY Australia Post Reply Pai', 67: 'BARCODE_AUSROUTE Australia Post Routin', 68: 'BARCODE_AUSREDIRECT Australia Post Redirectio', 69: 'BARCODE_ISBNX ISBN (EAN-13 with verification stage', 70: 'BARCODE_RM4SCC Royal Mail 4 State (RM4SCC', 71: 'BARCODE_DATAMATRIX Data Matri', 72: 'BARCODE_EAN14 EAN-1', 75: 'BARCODE_NVE18 NVE-1', 76: 'BARCODE_JAPANPOST Japanese Pos', 77: 'BARCODE_KOREAPOST Korea Pos', 79: 'BARCODE_RSS14STACK GS1 DataBar-14 Stacke', 80: 'BARCODE_RSS14STACK_OMNI GS1 DataBar-14 Stacked Omnidirectiona', 81: 'BARCODE_RSS_EXPSTACK GS1 DataBar Expanded Stacke', 82: 'BARCODE_PLANET PLANE', 84: 'BARCODE_MICROPDF417 MicroPDF41', 85: 'BARCODE_ONECODE USPS OneCod', 86: 'BARCODE_PLESSEY Plessey Cod', 87: 'BARCODE_TELEPEN_NUM Telepen Numeri', 89: 'BARCODE_ITF14 ITF-1', 90: 'BARCODE_KIX Dutch Post KIX Cod', 92: 'BARCODE_AZTEC Aztec Cod', 93: 'BARCODE_DAFT DAFT Cod', 97: 'BARCODE_MICROQR Micro QR Cod', 98: 'BARCODE_HIBC_128 HIBC Code 12', 99: 'BARCODE_HIBC_39 HIBC Code 3', 102: 'BARCODE_HIBC_DM HIBC Data Matri', 104: 'BARCODE_HIBC_QR HIBC QR Cod', 106: 'BARCODE_HIBC_PDF HIBC PDF41', 108: 'BARCODE_HIBC_MICPDF HIBC MicroPDF41', 112: 'BARCODE_HIBC_AZTEC HIBC Aztec Cod', 128: 'BARCODE_AZRUNE Aztec Rune', 129: 'BARCODE_CODE32 Code 3', 130: 'BARCODE_EANX_CC Composite Symbol with EAN linear componen', 131: 'BARCODE_EAN128_CC Composite Symbol with GS1-128 linear componen', 132: 'BARCODE_RSS14_CC Composite Symbol with GS1 DataBar-14 linear componen', 133: 'BARCODE_RSS_LTD_CC Composite Symbol with GS1 DataBar Limited componen', 134: 'BARCODE_RSS_EXP_CC Composite Symbol with GS1 DataBar Extended componen', 135: 'BARCODE_UPCA_CC Composite Symbol with UPC A linear componen', 136: 'BARCODE_UPCE_CC Composite Symbol with UPC E linear componen', 137: 'BARCODE_RSS14STACK_CC Composite Symbol with GS1 DataBar-14 Stacked componen', 138: 'BARCODE_RSS14_OMNI_CC Composite Symbol with GS1 DataBar-14 Stacked Omnidirectional componen', 139: 'BARCODE_RSS_EXPSTACK_CC Composite Symbol with GS1 DataBar Expanded Stacked componen', 140: 'BARCODE_CHANNEL Channel Cod', 141: 'BARCODE_CODEONE Code On', 142: 'BARCODE_GRIDMATRIX Grid Matr'}
-            Object.keys(BARCODE_TYPES).map((idx)=>{
-                let opt = document.createElement('option')
-                opt.value = idx
-                opt.innerHTML = BARCODE_TYPES[idx]
-                if(idx ==20)opt.setAttribute('selected','');
-                this.barcodeType.appendChild(opt)
-            })
+    generateBarcodeTypes(){
+        const BARCODE_TYPES = {1: 'BARCODE_CODE11 Code 1', 2: 'BARCODE_C25MATRIX Standard Code 2 of ', 3: 'BARCODE_C25INTER Interleaved 2 of ', 4: 'BARCODE_C25IATA Code 2 of 5 IAT', 6: 'BARCODE_C25LOGIC Code 2 of 5 Data Logi', 7: 'BARCODE_C25IND Code 2 of 5 Industria', 8: 'BARCODE_CODE39 Code 3 of 9 (Code 39', 9: 'BARCODE_EXCODE39 Extended Code 3 of 9 (Code 39+', 13: 'BARCODE_EANX EA', 16: 'BARCODE_EAN128 GS1-12', 18: 'BARCODE_CODABAR Codaba', 20: 'BARCODE_CODE128 Code 128 (automatic subset switching', 21: 'BARCODE_DPLEIT Deutshe Post Leitcod', 22: 'BARCODE_DPIDENT Deutshe Post Identcod', 23: 'BARCODE_CODE16K Code 16', 24: 'BARCODE_CODE49 Code 4', 25: 'BARCODE_CODE93 Code 9', 28: 'BARCODE_FLAT Flattermarke', 29: 'BARCODE_RSS14 GS1 DataBar-1', 30: 'BARCODE_RSS_LTD GS1 DataBar Limite', 31: 'BARCODE_RSS_EXP GS1 DataBar Expande', 32: 'BARCODE_TELEPEN Telepen Alph', 34: 'BARCODE_UPCA UPC ', 37: 'BARCODE_UPCE UPC ', 40: 'BARCODE_POSTNET PostNe', 47: 'BARCODE_MSI_PLESSEY MSI Plesse', 49: 'BARCODE_FIM FI', 50: 'BARCODE_LOGMARS LOGMAR', 51: 'BARCODE_PHARMA Pharmacode One-Trac', 52: 'BARCODE_PZN PZ', 53: 'BARCODE_PHARMA_TWO Pharmacode Two-Trac', 55: 'BARCODE_PDF417 PDF41', 56: 'BARCODE_PDF417TRUNC PDF417 Truncate', 57: 'BARCODE_MAXICODE Maxicod', 58: 'BARCODE_QRCODE QR Cod', 60: 'BARCODE_CODE128B Code 128 (Subset B', 63: 'BARCODE_AUSPOST Australia Post Standard Custome', 66: 'BARCODE_AUSREPLY Australia Post Reply Pai', 67: 'BARCODE_AUSROUTE Australia Post Routin', 68: 'BARCODE_AUSREDIRECT Australia Post Redirectio', 69: 'BARCODE_ISBNX ISBN (EAN-13 with verification stage', 70: 'BARCODE_RM4SCC Royal Mail 4 State (RM4SCC', 71: 'BARCODE_DATAMATRIX Data Matri', 72: 'BARCODE_EAN14 EAN-1', 75: 'BARCODE_NVE18 NVE-1', 76: 'BARCODE_JAPANPOST Japanese Pos', 77: 'BARCODE_KOREAPOST Korea Pos', 79: 'BARCODE_RSS14STACK GS1 DataBar-14 Stacke', 80: 'BARCODE_RSS14STACK_OMNI GS1 DataBar-14 Stacked Omnidirectiona', 81: 'BARCODE_RSS_EXPSTACK GS1 DataBar Expanded Stacke', 82: 'BARCODE_PLANET PLANE', 84: 'BARCODE_MICROPDF417 MicroPDF41', 85: 'BARCODE_ONECODE USPS OneCod', 86: 'BARCODE_PLESSEY Plessey Cod', 87: 'BARCODE_TELEPEN_NUM Telepen Numeri', 89: 'BARCODE_ITF14 ITF-1', 90: 'BARCODE_KIX Dutch Post KIX Cod', 92: 'BARCODE_AZTEC Aztec Cod', 93: 'BARCODE_DAFT DAFT Cod', 97: 'BARCODE_MICROQR Micro QR Cod', 98: 'BARCODE_HIBC_128 HIBC Code 12', 99: 'BARCODE_HIBC_39 HIBC Code 3', 102: 'BARCODE_HIBC_DM HIBC Data Matri', 104: 'BARCODE_HIBC_QR HIBC QR Cod', 106: 'BARCODE_HIBC_PDF HIBC PDF41', 108: 'BARCODE_HIBC_MICPDF HIBC MicroPDF41', 112: 'BARCODE_HIBC_AZTEC HIBC Aztec Cod', 128: 'BARCODE_AZRUNE Aztec Rune', 129: 'BARCODE_CODE32 Code 3', 130: 'BARCODE_EANX_CC Composite Symbol with EAN linear componen', 131: 'BARCODE_EAN128_CC Composite Symbol with GS1-128 linear componen', 132: 'BARCODE_RSS14_CC Composite Symbol with GS1 DataBar-14 linear componen', 133: 'BARCODE_RSS_LTD_CC Composite Symbol with GS1 DataBar Limited componen', 134: 'BARCODE_RSS_EXP_CC Composite Symbol with GS1 DataBar Extended componen', 135: 'BARCODE_UPCA_CC Composite Symbol with UPC A linear componen', 136: 'BARCODE_UPCE_CC Composite Symbol with UPC E linear componen', 137: 'BARCODE_RSS14STACK_CC Composite Symbol with GS1 DataBar-14 Stacked componen', 138: 'BARCODE_RSS14_OMNI_CC Composite Symbol with GS1 DataBar-14 Stacked Omnidirectional componen', 139: 'BARCODE_RSS_EXPSTACK_CC Composite Symbol with GS1 DataBar Expanded Stacked componen', 140: 'BARCODE_CHANNEL Channel Cod', 141: 'BARCODE_CODEONE Code On', 142: 'BARCODE_GRIDMATRIX Grid Matr'}
+        Object.keys(BARCODE_TYPES).map((idx)=>{
+            let opt = document.createElement('option')
+            opt.value = idx
+            opt.innerHTML = BARCODE_TYPES[idx]
+            if(idx ==20)opt.setAttribute('selected','');
+            this.barcodeType.appendChild(opt)
+        })
+    }
+    hasCErrors(data){
+        //* data = {[bitmap, bitmap_width, bitmap_height, bitmap_size, errors]} *// 
+        let errors = data[0][4]
+        this.safeReset(this.errorText)
+        this.safeReset(this.output_wrapper)
+        if(errors && errors.length){
+            this.errorText.innerText = errors;
+            this.wrapper.appendChild(this.errorText)
+            return true
         }
-        handleCErrors(data){
-            //* data = {[bitmap, bitmap_width, bitmap_height, bitmap_size, errors]} *// 
-            try{this.wrapper.removeChild(this.errorText)}catch{}
-            if(data[0][4].length){
-                this.errorText.innerText = data[0][4];
-                this.wrapper.appendChild(this.errorText)
-            }
-        }
+        return false
+    }
+    safeReset(elm){
+        try{this.wrapper.removeChild(elm) }catch{}
+    }
 
 }
